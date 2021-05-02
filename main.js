26827768769939082af6553b95f0e6a162ff835b
Blake Purdue <blake.purdue@gmail.com>
2021-05-02 20:18:20 +1200

    Basic Workout of Website

---
+++ main.js
@@ -0,0 +1,13 @@
+class Pizza {
+    constructor(size, sizePrice,baseType, baseTypePrice, typePizza, typePizzaPrice, extras, extraPrice, totalPrice) {
+      this.size = size;
+      this.sizePrice = sizePrice;
+      this.baseType = baseType;
+      this.baseTypePrice = baseTypePrice;
+      this.typePizza = typePizza;
+      this.typePizzaPrice = typePizzaPrice;
+      this.Extras = extras;
+      this.extraPrice = extraPrice;
+      this.totalPrice = totalPrice;
+    }
+  }
