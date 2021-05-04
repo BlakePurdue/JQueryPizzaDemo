a69d57dc6cc5a816edc6e51936dcb1b5cdebddc8
Blake Purdue <blake.purdue@gmail.com>
2021-05-04 18:40:45 +1200

    Added some JS to make it work

--- main.js
+++ main.js
@@ -11,3 +11,110 @@ class Pizza {
       this.totalPrice = totalPrice;
     }
   }
+
+  let pizzaTypeMap = new Map();
+  let pizzaSizeMap = new Map();
+  let pizzaBaseMap = new Map();
+
+  let localCartPizzas = [];
+  let localTotalPrice = 0.00;
+
+// Function to run on load to populate Maps 
+$(function() {
+    //Pizza Type Content
+    pizzaTypeMap.set("Pepperoni", "3.00");
+    pizzaTypeMap.set("Hawaiian", "3.00");
+    pizzaTypeMap.set("Classic Cheese", "3.00");
+    pizzaTypeMap.set("BBQ Chicken & Bacon Deluxe", "8.00");
+
+    // Pizza Size Content
+    pizzaSizeMap.set("Small", "2.50");
+    pizzaSizeMap.set("Medium", "5.00");
+    pizzaSizeMap.set("Large", "8.00");
+
+    // Pizza Base Content
+    pizzaBaseMap.set("Regular", "0.00");
+    pizzaBaseMap.set("Thin crust", "1.00");
+    pizzaBaseMap.set("Cheesy crust", "2.00");
+    pizzaBaseMap.set("New York Style *Limited Time*", "3.50");
+
+    $("#priceTotal").html("$ "+ localTotalPrice);
+
+    console.log("Pizza Stuff added");
+});
+
+
+// Pizza Order Button
+$("#pizzaSubmitBtn").click(function(){
+
+  var typePizza = $( "#pizzaTypeSelect" ).val();
+  var typePizzaPrice = pizzaTypeMap.get(typePizza);
+
+  var sizePizza = $( "#pizzaSizeSelect" ).val();
+  var sizePizzaPrice = pizzaSizeMap.get(sizePizza);
+
+  var basePizza = $( "#pizzaBaseSelect" ).val();
+  var basePizzaPrice = pizzaBaseMap.get(basePizza);
+
+  var checkTest = $('.checkTest:checkbox:checked');
+
+  var extrasArray = [];
+
+  $.each($("input[name='extraOption']:checked"), function(){
+    extrasArray.push($(this).val());
+  });
+  var extraPrice = parseInt(extrasArray.length) * 2; // Number of extras * 2, each extra is $2
+
+  var totalPrice = parseFloat(typePizzaPrice) + parseFloat(sizePizzaPrice) + parseFloat(basePizzaPrice) + parseFloat(extraPrice);
+
+  var newPizza = new Pizza(
+    sizePizza,
+    sizePizzaPrice,
+    basePizza,
+    basePizzaPrice,
+    typePizza,
+    typePizzaPrice,
+    extrasArray,
+    extraPrice,
+    totalPrice,
+  );
+  
+  addPizzaToCart(newPizza);
+
+  // Clear form once added to cart
+  document.getElementById("pizzaForm").reset();
+});
+
+function addPizzaToCart(pizza){
+  //Add pizza to array
+  localCartPizzas.add(pizza);
+  // Update list of pizza
+  addContent();
+
+  // Update total price
+  localTotalPrice = localTotalPrice + pizza.totalPrice;
+  // Update total price on html
+  $("#priceTotal").html("$ "+localTotalPrice);
+}
+
+
+function addContent(){
+  for(var i =0; i< localCartPizzas.lenght; i ++){
+    
+  }
+
+
+  var txt1Line = pizza.size + " " + pizza.baseType + " " + pizza.typePizza;
+  var txt1 = $("<h6></h6>").text(txt1Line);
+  var ulPart1 = $("<ul>");
+  var line = $("<li></li>").text(txt1Line);
+  var ulPart2 = $("</ul>");
+
+
+  // Add content to page
+  $("#groupOfPizza").append(txt1,ulPart1,line, ulPart2);  
+}
+
+$("#couponBtn").click(function(){
+  console.log("Coip");
+});
